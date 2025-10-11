3aa7e5acdccd2859190cd365251b8bddaa9cc1cc
David Adame <davidadavaz@outlook.com>
2025-10-11 13:11:17 -0600

    added post methods (login and signup)

--- playground/urls.py
+++ playground/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
 
+app_name = "playground"
 #URLConf
 urlpatterns=[
-    path("",views.welcome_playground),
-    path("hello/",views.say_hello),
-    path("operations/",views.operation),
+    path("",views.welcome_playground,name="playground"),
+    path("hello/",views.say_hello,name="hello"),
+    path("operations/",views.operation,name="operations"),
     path("operations/result/",views.get_result, name="result"),
-    path("home/",views.base) 
+    path("home/",views.base,name="home"),
+    path("login/",views.login_page,name="login"),
+    path("lobby/",views.login,name="lobby"),
+    path("signup/",views.signup_page,name="signup"),
+    path("signup/success",views.signup,name="success")
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+import database as db
 # Create your views here.
 # request handler
 #action
@@ -34,5 +36,42 @@ def get_result(request):
             res=n1/n2
     return render(request,"result.html",{"result":res})
 
+def login_page(request):
+    return render(request,"login.html")
+
+
 def login(request): #Post Method
-    pass
+    error=None
+    state=False
+    try:
+        user=request.POST.get("user")
+        password=request.POST.get("password")
+        if db.retrieve_user(user)["exists"]:
+            if db.verify_credentials(user,password)["access_granted"]:
+                state=True
+            else:
+                state=False
+        else:
+            state=False
+    except Exception as e:
+        error = str(e)
+    return render(request, "lobby.html",{"state":state,"error":error})
+
+def signup_page(request):
+    return render(request,"signup.html")
+
+def signup(request):
+    try:
+        name=request.POST["name"]
+        user=request.POST["user"]
+        password=request.POST["password"]
+        reg=False
+        if not db.retrieve_user(user)["exists"]:
+            db.add_user(name,user,password)
+            message="User Registered correctly"
+            reg=True
+        else:
+            message="Awww! username already taken :("
+    except Exception as e:
+        message=f"Te following error(s) occurred:{e}"
+    return render(request,"success.html",{"registered":reg,"message":message})
